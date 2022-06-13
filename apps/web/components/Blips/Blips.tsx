e1106ae3a1672fcc3bc7933d0a152a596cdf50e4
Alan Russell <ajrussellaudio@gmail.com>
2022-06-13 22:09:49 +0100

    Scale grid to give all segments equal area

--- apps/web/components/Blips/Blips.tsx
+++ apps/web/components/Blips/Blips.tsx
@@ -10,18 +10,35 @@ export function Blips() {
   const { height, width, radius } = useSize();
   const { random } = useRandom();
 
-  function randomInSector(blipSector: Sector, sectors: Sector[], scaling: number) {
-    const padding = scaling / 20;
-    const index = sectors.findIndex((sector) => sector._id === blipSector._id);
-    return random(padding + (index / sectors.length) * scaling, ((index + 1) / sectors.length) * scaling - padding);
+  function randomInSector(blipQuadrant: Sector) {
+    const TWO_PI = Math.PI * 2;
+    const padding = TWO_PI / 20;
+    const index = quadrants.findIndex((quadrant) => quadrant._id === blipQuadrant._id);
+    const bound = (index: number) => {
+      return (index / quadrants.length) * TWO_PI;
+    };
+    const lowerBound = bound(index) + padding;
+    const upperBound = bound(index + 1) - padding;
+    return random(lowerBound, upperBound);
+  }
+
+  function randomInRing(blipRing: Sector) {
+    const padding = radius / 20;
+    const index = rings.findIndex((ring) => ring._id === blipRing._id);
+    const bound = (index: number) => {
+      return Math.sqrt(index / rings.length) * radius;
+    };
+    const lowerBound = bound(index) + padding;
+    const upperBound = bound(index + 1) - padding;
+    return random(lowerBound, upperBound);
   }
 
   return (
     <Group top={height / 2} left={width / 2}>
       {blips.map((blip) => {
         const { x, y } = cartesian({
-          angle: randomInSector(blip.quadrant, quadrants, Math.PI * 2),
-          radius: randomInSector(blip.ring, rings, radius),
+          angle: randomInSector(blip.quadrant),
+          radius: randomInRing(blip.ring),
         });
 
         return <Blip key={blip._id} x={x} y={y} radius={radius / 50} title={blip.name} />;
--- apps/web/components/Grid/Grid.tsx
+++ apps/web/components/Grid/Grid.tsx
@@ -3,8 +3,17 @@ import { GridAngle, GridRadial } from '@visx/grid';
 import { Group } from '@visx/group';
 import { useRadarContext } from '../../context/Radar';
 import { useSize } from '../../context/Size';
+import { Circle } from '@visx/shape';
 
 export type GridProps = Record<string, never>;
+
+const style = {
+  stroke: 'black',
+  strokeWidth: 1,
+  strokeOpacity: 0.3,
+  strokeDasharray: '4,3',
+  fill: 'none',
+};
 
 export function Grid() {
   const { quadrants, rings } = useRadarContext();
@@ -14,32 +23,14 @@ export function Grid() {
     domain: quadrants.map((quadrant) => quadrant.name),
     range: [0, Math.PI * 2],
   });
-  const ringScale = scaleBand({
-    domain: rings.map((ring) => ring.name),
-    range: [0, yMax / 2],
-  });
 
   return (
     <Group top={height / 2} left={width / 2}>
-      <GridAngle outerRadius={radius} scale={quadrantScale} stroke="black" strokeWidth={1} strokeOpacity={0.3} />
-      <GridRadial
-        scale={ringScale}
-        data={rings}
-        stroke="black"
-        strokeWidth={1}
-        strokeOpacity={0.3}
-        strokeDasharray="5,2"
-      />
-      <circle
-        cx={0}
-        cy={0}
-        r={radius}
-        stroke="black"
-        strokeWidth={1}
-        strokeOpacity={0.3}
-        strokeDasharray="5,2"
-        fill="none"
-      />
+      <GridAngle outerRadius={radius} scale={quadrantScale} {...style} />
+      {rings.map((ring, i) => {
+        const r = radius * Math.sqrt((i + 1) / rings.length);
+        return <Circle key={ring._id} cx={0} cy={0} r={r} {...style} />;
+      })}
     </Group>
   );
 }
